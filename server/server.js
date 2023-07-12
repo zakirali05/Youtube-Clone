1a5002948203340b67466230c4135a432b23dad8
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2023-07-12 12:33:37 +0530

    app.js setup done

--- server/server.js
+++ server/server.js
@@ -1,4 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: __dirname + "/.env" });
+
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/youtube-clone")
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on localhost:${process.env.PORT}`);
+});
 /*
+
 server(folder)
     server.js(file)
     .env (file)
