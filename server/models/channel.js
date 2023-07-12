8e01743e9c1c8cb623e1ca3d2279c3e297294b3a
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2023-07-12 17:38:42 +0530

    Timestamps added on models

--- server/models/channel.js
+++ server/models/channel.js
@@ -1,37 +1,40 @@
 const mongoose = require("mongoose");
 
-const channelSchema = new mongoose.Schema({
-  profileImage: {
-    public_id: { type: String, required: true },
-    url: { type: String, required: true },
-  },
-  profilePoster: {
-    public_id: { type: String, required: true },
-    url: { type: String, required: true },
-  },
-  channelName: {
-    type: String,
-    required: true,
-  },
-  channelDescription: {
-    type: String,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  subscribers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  videos: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Video",
+const channelSchema = new mongoose.Schema(
+  {
+    profileImage: {
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
     },
-  ],
-  totalView: {
-    type: Number,
-    default: 0,
+    profilePoster: {
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
+    },
+    channelName: {
+      type: String,
+      required: true,
+    },
+    channelDescription: {
+      type: String,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    subscribers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    videos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    totalView: {
+      type: Number,
+      default: 0,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Channel = mongoose.model("Channel", channelSchema);
 module.exports = Channel;
--- server/models/video.js
+++ server/models/video.js
@@ -1,56 +1,59 @@
 const mongoose = require("mongoose");
 
-const videoSchema = new mongoose.Schema({
-  thumbnail: {
-    public_id: { type: String, required: true },
-    url: { type: String, required: true },
-  },
-  video: {
-    public_id: { type: String, required: true },
-    url: { type: String, required: true },
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  genre: [{ type: String, required: true }],
-  channel: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Channel",
-  },
-  views: {
-    type: Number,
-    default: 0,
-  },
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+const videoSchema = new mongoose.Schema(
+  {
+    thumbnail: {
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
     },
-  ],
-  dislikes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+    video: {
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
     },
-  ],
-
-  comments: [
-    {
-      user: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    genre: [{ type: String, required: true }],
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Channel",
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    likes: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
       },
-      comment: {
-        type: String,
-        required: true,
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
       },
-    },
-  ],
-});
+    ],
+
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const Video = mongoose.model("Video", videoSchema);
 module.exports = Video;
